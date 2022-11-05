fba3a2f029da032849f9e0cee140566e4e24d970
manjimaprasad22 <manjimaprasad22@gmail.com>
2022-11-05 13:45:53 +0530

    myfisrtpage

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ import Test from './Test';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   {/* <RouteComp/> */}
-   <Test/>
+   <RouteComp/>
+   {/* <Test/> */}
   </React.StrictMode>
 );
 
